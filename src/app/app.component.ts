e1626b2fc12ebf532a63172aa228eba3f95de610
Lena Portianko <lenaportianko@gmail.com>
2023-02-25 19:07:40 +0200

    Set title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'lenaportianko.github.io';
+export class AppComponent implements OnInit {
+
+  protected title = 'Lena Portianko`s personal website';
+
+  constructor(
+    protected titleService: Title
+  ) {}
+  
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+  }
 }
